22ef3c683517cd80567ea6a6bb5e29cec2c0a336
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2023-09-06 22:19:41 -0300

    update tailwindcss  breeze+react

--- laravel-react-breeze/resources/js/Pages/Welcome.jsx
+++ laravel-react-breeze/resources/js/Pages/Welcome.jsx
@@ -4,13 +4,13 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
         <>
             <Head title="Welcome" />
-            <div className="flex flex-col justify-center items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-                <h1 className='text-6xl text-white'>Gerenciador de Tarefas</h1>
+            <div className="flex flex-col justify-center items-center min-h-screen bg-dots-darker bg-center bg-gray-100 selection:text-white">
+                <h1 className='text-6xl  text-black'>Gerenciador de Tarefas</h1>
                 <div className="p-6 text-right">
                     {auth.user ? (
                         <Link
                             href={route('dashboard')}
-                            className="duration-300 bg-black px-4 py-2 rounded-md text-lg font-semibold text-white hover:text-black hover:bg-white transition :hover:text-white"
+                            className="duration-300 bg-black px-4 py-2 rounded-md text-lg font-semibold text-white hover:bg-gray-800 transition :hover:text-white"
                         >
                             Tarefas
                         </Link>
@@ -18,14 +18,14 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                         <>
                             <Link
                                 href={route('login')}
-                                className="duration-300 bg-black px-4 py-2 rounded-md text-lg font-semibold text-white hover:text-black hover:bg-white transition :hover:text-white"
+                                className="duration-300 bg-black px-4 py-2 rounded-md text-lg font-semibold text-white hover:bg-gray-800 transition :hover:text-white"
                             >
                                 Entrar
                             </Link>
 
                             <Link
                                 href={route('register')}
-                                className="duration-300 bg-black px-4 py-2 rounded-md text-lg ml-4 font-semibold text-white hover:text-black hover:bg-white transition"
+                                className="duration-300 bg-black px-4 py-2 rounded-md text-lg ml-4 font-semibold text-white hover:bg-gray-800 transition"
                             >
                                 Registrar
                             </Link>
