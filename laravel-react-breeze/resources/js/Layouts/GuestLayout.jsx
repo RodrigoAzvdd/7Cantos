7755fcccaf4da607e92fad088989ce380c172aca
rodrigo azevedo silva <rodrigo.azvd750@gmail.com>
2023-09-06 23:18:59 -0300

    update layoutPage

--- laravel-react-breeze/resources/js/Layouts/GuestLayout.jsx
+++ laravel-react-breeze/resources/js/Layouts/GuestLayout.jsx
@@ -4,6 +4,11 @@ import { Link } from '@inertiajs/react';
 export default function Guest({ children }) {
     return (
         <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+
+            <Link href='/' className='fixed top-10 left-10 px-7 py-2 bg-slate-700 rounded-lg text-white font-bold text-lg hover:bg-slate-500 transition duration-300 hover:scale-105'>
+                Voltar
+            </Link>
+
             <div>
                 <h1 className='font-bold text-5xl'>Login</h1>
             </div>
